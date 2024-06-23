dc346da6b02bbf66d0c027939495a2f8b9e00f05
no-names-html <vadim.atamanyuk1305.va@gmail.com>
2024-06-23 23:00:34 +0300

    second lesson

--- src/components/firstComponent.vue
+++ src/components/firstComponent.vue
@@ -1,13 +1,17 @@
 <template>
     <dir class="root">
         <p>Єба, це працює</p>
-        <button type="button" @click="delValue">-</button>
-        <input type='number' 
-            :value=numbersOfXyu 
-            @input='changeVal($event.target.value)'
-            @change='verify($event.target.value)'/>
-        <button type="button" @click="addValue">+</button>
-        <p>{{numbersOfXyu}}</p>
+        <p>Введіть ім'я користувача</p>
+        <input type="text" @input="selectedUsers = $event.target.value"/>
+        <div>
+            <div>Кількість користувачів {{ users.length }} </div>
+            <div>Кількість співпадінь {{ arrOfNames.length }} </div>
+        </div>
+        <ul>
+            <li
+            v-for="(people, index) in arrOfNames"
+            :key="index">{{people}}</li>
+        </ul>
     </dir>
 </template>
 
@@ -16,22 +20,13 @@ export default {
     name: 'firstComponent',
     data(){     
         return{
-            numbersOfXyu: 1
+            users: ['Vadim', 'Vikrotia', 'Vlad', 'Nata', 'Andryw', 'Vadim Atamanyuk'],
+            selectedUsers: ''
         }
     },
-    methods: {
-        addValue(){
-            this.numbersOfXyu += 1 ;
-        },
-        delValue(){
-            this.numbersOfXyu -= 1;
-        },
-        verify(val){
-            if(val>10) this.numbersOfXyu = 10;
-            else if(val<0) this.numbersOfXyu = 0;
-        },
-        changeVal(val){
-            this.numbersOfXyu = val; 
+    computed: {
+        arrOfNames(){
+            return this.users.filter(name => name.toLowerCase().includes(this.selectedUsers.toLowerCase()))
         }
     }      
 }
